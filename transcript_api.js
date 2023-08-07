f969583400ffa81b50e686cfb895193c385be01a
dhruv-mehrotra <dhruv_m@cs.iitr.ac.in>
2023-08-07 11:39:42 +0530

    feat: implemented transcription type based cleanup

--- transcript_api.js
+++ transcript_api.js
@@ -72,14 +72,30 @@ async function transcribe(transcript_type) {
     .map(result => result.alternatives[0].transcript)
     .join('\n');
   console.log(`Transcription: ${transcription}`);
-  cleanup(transcription + " ABC-123, Thomas Lane, New Delhi");
+  cleanup(transcription + " ABC-123, Thomas Lane, New Delhi", transcript_type);  // Added as an example address
 }
 
 // Transcription cleanup function
-async function cleanup(transcribed_text) {
+async function cleanup(transcribed_text, transcript_type) {
+
+  switch (transcript_type) {
+    case 'phone no':
+      gpt_prompt = "Reply only with a well formatted address, no other words by extracting a phone number from the given text: " + transcribed_text
+      break;
+    case 'address':
+      gpt_prompt = "Reply only with a well formatted address, no other words by extracting an address from the given text: " + transcribed_text
+      break;
+    case 'bank name':
+      gpt_prompt = "Reply only with a well formatted address, no other words by extracting a Bank name from the given text: " + transcribed_text
+      break;
+    case 'number':
+      gpt_prompt = "Reply only with a well formatted address, no other words by extracting a single digit number from the given text: " + transcribed_text
+      break;
+  }
+
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: "Reply only with a well formatted address, no other words by extracting an address from the given text: " + transcribed_text,
+    prompt: gpt_prompt,
   });
   console.log(transcribed_text);
   console.log(completion.data.choices[0].text);
